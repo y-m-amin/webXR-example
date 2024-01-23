5a4b548798564218bd7aab836a1589030cf2d491
Yousuf Mohammad Amin <ymohammad18@gmail.com>
2024-01-23 16:20:44 +0600

    test

---
+++ main.js
@@ -0,0 +1,27 @@
+import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+
+
+camera.position.z = 5;
+
+const loader = new GLTFLoader();
+
+loader.load( './public/aunkur_solo_test.glb', function ( gltf ) {
+
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
