34feff44eafdf3e22d8af6f2d58a7c145a22d19c
Yousuf Mohammad Amin <ymohammad18@gmail.com>
2024-01-25 15:30:47 +0600

    touch gesture test

--- XRGestures.js
+++ XRGestures.js
@@ -1,4 +1,4 @@
-import * as THREE from './three/three.module.js';
+import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r126/three.js';
 
 class XRGestures extends THREE.EventDispatcher{
     constructor( renderer ){
